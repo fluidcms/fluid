16b6220e078462eadad55e6a3c473b5619f52a8f
Gabriel Ames-Bull <g.ames-bull@tangente.co>
2013-04-30 09:59:32 -0400

    Moved page name variables from the structure file to the page file

--- src/Fluid/Models/Structure/LocalizeStructure.php
+++ src/Fluid/Models/Structure/LocalizeStructure.php
@@ -22,106 +22,51 @@ class LocalizeStructure
     }
 
     /**
-     * Localize file structure
+     * Localize the site structure.
      *
-     * @param   array   $structure
-     * @param   string  $language
-     * @return  void
+     * @param   array   $structure  The master structure
+     * @param   string  $language   The language to localize the structure to
+     * @param   string  $parent     The current page's parent
+     * @return  array   The localized structure
      */
-    public static function localize($structure, $language)
+    public static function localize($structure, $language, $parent = '')
     {
-        $localizedStructure = Storage::load("structure/structure_" . (isset($language) ? $language : self::getDefaultLanguage()) . ".json");
-
-        if ($language !== self::getDefaultLanguage()) {
-            $defaultStructure = Storage::load("structure/structure_" . self::getDefaultLanguage() . ".json");
+        $output = array();
+        foreach ($structure as $page) {
+            if (in_array($language, $page['languages'])) {
+                $name = self::getLocalizedName($parent, $page['page'], $language);
+                if (isset($page['pages']) && is_array($page['pages'])) {
+                    $newParent = trim($parent . "/" . $page['page'], '/');
+                    $pages = self::localize($page['pages'], $language, $newParent);
+                    $output[] = array_merge($page, array(
+                        "name" => $name,
+                        "pages" => $pages
+                    ));
+                } else {
+                    $output[] = array_merge($page, array("name" => $name));
+                }
+            }
         }
 
-        $structure = self::removeUnlocalizedPages($structure, $language);
-
-        $structure = self::addLocalizedNames($structure, $localizedStructure, (isset($defaultStructure) ? $defaultStructure : null));
-
-        return $structure;
+        return $output;
     }
 
     /**
      * Remove pages that are not used localized
      *
-     * @param   array   $pages
-     * @param   string  $language
+     * @param   string  $parent     The current page's parent
+     * @param   string  $page       The current page
+     * @param   string  $language   The language to localize the structure to
      * @return  array
      */
-    private static function removeUnlocalizedPages($pages, $language)
+    private static function getLocalizedName($parent, $page, $language)
     {
-        $output = array();
-
-        foreach ($pages as $page) {
-            if (isset($page['languages']) && !in_array($language, $page['languages'])) {
-                continue;
-            }
-
-            if (isset($page['pages']) && is_array($page['pages'])) {
-                $page['pages'] = self::removeUnlocalizedPages($page['pages'], $language);
-            }
-
-            $output[] = $page;
+        if (!empty($parent)) {
+            $parent .= '/';
         }
 
-        return $output;
-    }
+        $page = Storage::load('pages/' . $parent . $page . '_' . $language . ".json");
 
-    /**
-     * Add localized name to the pages or fallback to the default language name
-     *
-     * @param   array   $pages
-     * @param   array   $localizedPages
-     * @param   array   $defaultPages
-     * @return  array
-     */
-    public static function addLocalizedNames($pages, $localizedPages = null, $defaultPages = null)
-    {
-        $output = array();
-
-        foreach ($pages as $page) {
-            // Search page in localized pages
-            $localizedPage = null;
-            if (isset($localizedPages)) {
-                foreach ($localizedPages as $item) {
-                    if (isset($item['page']) && $page['page'] === $item['page']) {
-                        $localizedPage = $item;
-                        break;
-                    }
-                }
-            }
-            // Search page in default pages
-            $defaultPage = null;
-            if (isset($defaultPages)) {
-                foreach ($defaultPages as $item) {
-                    if (isset($item['page']) && $page['page'] === $item['page']) {
-                        $defaultPage = $item;
-                        break;
-                    }
-                }
-            }
-
-            if (!empty($localizedPage['name'])) {
-                $page['name'] = $localizedPage['name'];
-            } else if (isset($defaultPages) && !empty($defaultPage['name'])) {
-                $page['name'] = $defaultPage['name'];
-            } else {
-                $page['name'] = '';
-            }
-
-            if (isset($page['pages']) && is_array($page['pages'])) {
-                $page['pages'] = self::addLocalizedNames(
-                    $page['pages'],
-                    (isset($localizedPage['pages']) ? $localizedPage['pages'] : null),
-                    (isset($defaultPage['pages']) ? $defaultPage['pages'] : null)
-                );
-            }
-
-            $output[] = $page;
-        }
-
-        return $output;
+        return $page['name'];
     }
 }
