28d7b5b4d4928e2ac06f93b80535ee4f2d5f657b
Gabriel Bull <gavroche.bull@gmail.com>
2013-09-09 21:54:12 -0400

    Added test for images in array

--- test/Fluid/Tests/Files/EditImageTest.php
+++ test/Fluid/Tests/Files/EditImageTest.php
@@ -46,6 +46,35 @@ class EditImageTest extends PHPUnit_Framework_TestCase
         $this->assertFileExists(preg_replace('!^/fluidcms/images!', $dir, $retval['data']['Site']['Logo']['Retina']['src']));
     }
 
+    public function testEditImageInArray()
+    {
+        $page = Page::get(null, 'en-US');
+
+        $data = $page->getRawData();
+
+        $data['Site']['MyArray'][]['Image'] = '0i7ygv3r';
+
+        $request = array(
+            "method" => "PUT",
+            "url" => "page/en-US/global",
+            "data" => $data
+        );
+
+        ob_start();
+        new Fluid\WebSockets\Requests($request['url'], $request['method'], $request['data'], 'develop', Helper::getUser());
+        $retval = ob_get_contents();
+        ob_end_clean();
+
+        $retval = json_decode($retval, true);
+
+        $this->assertEquals(300, $retval['data']['Site']['MyArray'][0]['Image']['width']);
+        $this->assertEquals(86, $retval['data']['Site']['MyArray'][0]['Image']['height']);
+        $this->assertEquals('/fluidcms/images/0i7ygv3r/Logo.png', $retval['data']['Site']['MyArray'][0]['Image']['src']);
+
+        $dir = Fluid\Fluid::getBranchStorage() . "files";
+        $this->assertFileExists(preg_replace('!^/fluidcms/images!', $dir, $retval['data']['Site']['MyArray'][0]['Image']['src']));
+    }
+
     public function tearDown()
     {
         Helper::deleteStorage();
