28ab0a212ec9831887d2bb73d4e9fd15dbdbb1d5
Gabriel Bull <gavroche.bull@gmail.com>
2013-11-04 21:23:41 -0500

    Updated tests

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,8 +4,18 @@ date_default_timezone_set("America/Montreal");
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-$loader = require __DIR__ . '/../../../autoload.php';
-$loader->register();
+$vendor = realpath(__DIR__ . '/../vendor');
+
+if (file_exists($vendor . "/autoload.php")) {
+    require $vendor . "/autoload.php";
+} else {
+    $vendor = realpath(__DIR__ . '/../../../');
+    if (file_exists($vendor . "/autoload.php")) {
+        require $vendor . "/autoload.php";
+    } else {
+        throw new Exception("Unable to load dependencies");
+    }
+}
 
 require_once __DIR__ . '/helper.php';
 
