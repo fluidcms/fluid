146a4f6e6800c6e74fae9ac60155a1344a74bece
Gabriel Bull <me@gabrielbull.com>
2014-05-15 13:19:12 -0400

    Renamed layout config

--- src/Fluid/TemplateEngineInterface.php
+++ src/Fluid/TemplateEngineInterface.php
@@ -1,21 +1,23 @@
 <?php
 namespace Fluid;
 
+use Fluid\Template\TemplateConfig;
+
 interface TemplateEngineInterface
 {
     /**
      * @param string $template
      * @param array $data
-     * @param \Fluid\Layout\Config $config
+     * @param TemplateConfig $config
      * @return string
      */
-    public function render($template, array $data, Layout\Config $config);
+    public function render($template, array $data, TemplateConfig $config);
 
     /**
      * @param string $template
      * @param array $data
-     * @param \Fluid\Layout\Config $config
+     * @param TemplateConfig $config
      * @return string
      */
-    public function renderCompontent($template, array $data, Layout\Config $config);
+    public function renderCompontent($template, array $data, TemplateConfig $config);
 }
